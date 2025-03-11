e882b0daa501ed32c3b287e6e6bbce5b6d35480b
mayanksingh09 <mayanksingh@grubhub.com>
2025-03-11 10:46:34 -0400

    Updated neotree keymap

--- init.lua
+++ init.lua
@@ -985,7 +985,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.neo-tree',
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
@@ -1019,6 +1019,7 @@ require('lazy').setup({
     },
   },
 })
+vim.keymap.set('n', '<leader>e', '<Cmd>Neotree toggle<CR>')
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
