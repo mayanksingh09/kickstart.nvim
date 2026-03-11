e60c747e2d7854854620d7797ad72f0525483340
Mayank Singh <mayanksingh09@outlook.com>
2026-03-11 17:48:29 -0400

    Added nvim scrollbar

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,15 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'petertriho/nvim-scrollbar',
+    dependencies = {
+      'lewis6991/gitsigns.nvim',
+    },
+    config = function()
+      require('scrollbar').setup()
+      require('scrollbar.handlers.gitsigns').setup()
+    end,
+  },
+}
